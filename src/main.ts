294c6cb6d6d39eaec404eb4f828bf08f7e546c3a
knorien <zhangjinwei0320@hotmail.com>
2020-07-07 11:32:51 +0800

    pkg: upgrade deps & devDeps

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue';
 
 // rfcs https://github.com/vuejs/rfcs/tree/master/actzive-rfcs
 // 所有的特性 https://github.com/vuejs/rfcs/pulls?q=is%3Apr+is%3Amerged+label%3A3.x
+// 文档暂时可以参考composition api https://composition-api.vuejs.org/#basic-example
 createApp(App)
   .use(router)
   .use(store)
