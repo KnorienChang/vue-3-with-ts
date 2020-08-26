92a3d39fc38285d191a87624582d08d014f6bae6
knorien <zhangjinwei0320@hotmail.com>
2020-08-26 18:03:11 +0800

    declare: ComponentOptions ---> defineComponent

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
-declare module '*.vue' {
-  import { ComponentOptions } from 'vue';
-  const component: ComponentOptions;
+declare module "*.vue" {
+  import { defineComponent } from "vue";
+  const component: ReturnType<typeof defineComponent>;
   export default component;
 }
